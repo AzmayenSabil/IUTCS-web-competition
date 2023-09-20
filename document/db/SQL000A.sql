23bb4909877ce9e5df4a9d7c8bcea27f768e7ca8
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-09-20 23:42:06 +0600

    document folder added

--- document/db/SQL000A.sql
+++
@@ -1,7 +0,0 @@
-USE dms_db;
-CREATE TABLE `scripts` (
-                           script_id BIGINT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-                           script_name VARCHAR(15) NOT NULL,
-                           createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           PRIMARY KEY (`script_id`)
-);
