2f19779dca1792720dd6a07e09b502453253f4b5
Sheikh-Tafsir <190041130tafsir@gmail.com>
2023-09-23 12:26:39 +0600

    achievements almost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LandigPage from './pages/landingPage/LandigPage'
 import About from './pages/about/About'
+import Achievements from './pages/achievements/Achievements'
 
 const App = () => {
   return (
     <>
-      <LandigPage/>
+      {/* <LandigPage/> */}
       {/* <About /> */}
+      <Achievements />
     </>
   )
 }
